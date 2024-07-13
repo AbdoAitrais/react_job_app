bf0a048a28a4da4ca3aa892696324e69737c34bf
AbdoAitrais <abdo-pro@hotmail.com>
2024-07-13 13:50:45 +0100

    Initial commit for ts version

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import NavBar from "./components/NavBar.jsx";
+import Hero from "./components/Hero.jsx";
+import HomeCards from "./components/HomeCards.jsx";
+import JobListings from "./components/JobListings.jsx";
+import ViewAllJobs from "./components/ViewAllJobs.jsx";
+
+const App = () => {
+    return (
+        <>
+            <NavBar/>
+            <Hero/>
+            <HomeCards/>
+            <JobListings/>
+            <ViewAllJobs/>
+
+        </>
+    )
+}
+export default App
+
