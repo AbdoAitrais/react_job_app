ab13df54e056d25a2aaf79da1a4675b618ef2e7a
AbdoAitrais <abdo-pro@hotmail.com>
2024-07-15 15:47:17 +0100

    added single job page

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,15 @@ import MainLayout from './layouts/MainLayout.js';
 import HomePage from './pages/HomePage.js';
 import JobsPage from './pages/JobsPage.js';
 import NotFoundPage from './pages/NotFoundPage.js';
+import JobPage from './pages/JobPage.js';
+import jobLoader from './data-loaders/JobLoader.js';
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path='/' element={<MainLayout/>}>
             <Route index element={<HomePage/>}></Route>
             <Route path='/jobs' element={<JobsPage/>}></Route>
+            <Route path='/jobs/:id' element={<JobPage/>} loader={jobLoader}></Route>
             <Route path='*' element={<NotFoundPage/>}></Route>
         </Route>
     )
--- src/components/JobListings.tsx
+++ src/components/JobListings.tsx
@@ -1,20 +1,45 @@
-import jobs from '../jobs.json';
+import { useEffect, useState } from "react";
 import JobListing from "./JobListing.js";
+import Spinner from "./Spinner.js";
 
 const JobListings = ({isHomePage = false}: {isHomePage:boolean}) => {
-    const JobListings = isHomePage ? jobs.slice(0,3) : jobs;
+    const [jobs, setJobs] = useState([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        const fetchJobs = async () => {
+            const apiUrl = isHomePage 
+            ? '/api/jobs?_limit=3' 
+            : '/api/jobs';
+            try{
+                const res = await fetch(apiUrl);
+                const data = await res.json();
+                setJobs(data);
+            } catch(error) {
+                console.log('Error fetching the jobs : ',error);
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchJobs();
+    }, [])
     return (
     <section className="bg-blue-50 px-4 py-10">
         <div className="container-xl lg:container m-auto">
             <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
                 {isHomePage ? 'Recent Jobs' : 'Browse Jobs'}
             </h2>
-            <div className="grid grid-col   s-1 md:grid-cols-3 gap-6">
-                {JobListings.map((job) => (
-                    <JobListing key={job.id} job={job}/>
-                ))}
+                {
+                    loading ? (
+                        <Spinner loading={loading}/>
+                    ) : (
+                        <div className="grid grid-col   s-1 md:grid-cols-3 gap-6">
+                            {jobs.map((job:any) => (
+                                <JobListing key={job.id} job={job}/>
+                            ))}
+                        </div>
+                    )
+                }
 
-            </div>
         </div>
     </section>
     )
